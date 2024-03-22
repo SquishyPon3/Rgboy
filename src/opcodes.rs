a52cc0ec0da62fc69ceed177b643545c524e3afe
Kitt3 <vinylshock@mail.com>
2024-03-21 22:29:06 -0700

    opcodes necessary for previous commit

---
+++ src/opcodes.rs
@@ -0,0 +1,8 @@
+pub struct OpcodeLDA {
+    pub IMMEDIATE_0xA9: u8,
+    pub ZERO_PAGE_0xA5: u8
+}
+pub const OPCODE_LDA: OpcodeLDA = OpcodeLDA {
+    IMMEDIATE_0xA9: 0xA9,
+    ZERO_PAGE_0xA5: 0xA5,
+};
