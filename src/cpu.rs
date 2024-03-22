cd65db4fe0ab9ac30f697d8701ad5d913da509ed
Kitt3 <vinylshock@mail.com>
2024-03-21 21:57:32 -0700

    CPU now has rom mem array of u8 and loads from input program

--- src/cpu.rs
+++ src/cpu.rs
@@ -7,57 +7,115 @@ pub struct CPU {
     pub register_x: u8,
     pub status: u8,
     pub counter: u16,
+    // [0x80000 .. 0xFFFF] Program ROM
+    memory: [u8; PGRM_ROM_END as usize]
 }
 
+// Beginning and end of the available Program ROM memory
+const PGRM_ROM_START: u16 = 0x8000;
+const PGRM_ROM_END: u16 = 0xFFFF;
+// Address stored within cartridge which indicates where execution begins
+const PGRM_START_ADDR: u16 = 0xFFFC;
+
 impl  CPU {
+
     pub fn new() -> Self {
         CPU {
-            // Accumulator
             register_a: 0,
             register_x: 0,
             status: 0,
             counter: 0,
+            memory: [0; PGRM_ROM_END as usize]
         }
     }
 
-    pub fn interpret(&mut self, program: Vec<u8>) {
-        self.counter = 0;
+    fn mem_read(&self, addr: u16) -> u8 {
+        self.memory[addr as usize]
+    }
 
+    fn mem_read_u16(&self, pos: u16) -> u16 {
+        return u16::from_le_bytes(
+            [self.mem_read(pos), self.mem_read(pos + 1)]
+        );
+    }
+
+    fn mem_write(&mut self, addr: u16, data: u8) {
+        self.memory[addr as usize] = data;
+    }
+
+    fn mem_write_u16(&mut self, pos: u16, data: u16) {
+        let bytes = data.to_le_bytes();
+        self.mem_write(pos, bytes[0]);
+        self.mem_write(pos + 1, bytes[1]);
+    }
+
+    // Resets the state (register and flags) and sets counter to cart start addr
+    fn reset_interrupt(&mut self) {
+        // reset method should restore the state of all registers, and initialize program_counter by the 2-byte value stored at 0xFFFC
+        self.register_a = 0;
+        self.register_x = 0;
+        self.status = 0;
+        self.counter = self.mem_read_u16(PGRM_START_ADDR)
+    }
+
+    pub fn load(&mut self, program: Vec<u8>) {
+        // load method should load a program into PRG ROM space and save the reference to the code into 0xFFFC memory cell
+        self.memory[PGRM_ROM_START as usize .. (PGRM_ROM_START as usize + program.len())].copy_from_slice(&program[..]);
+        // Needs to be set to addr stored in start addr const
+        self.mem_write_u16(PGRM_START_ADDR, PGRM_ROM_START);
+    }
+
+    pub fn load_and_run(&mut self, program: Vec<u8>) {
+        self.load(program);
+        self.reset_interrupt();
+        self.run();
+    }
+
+    pub fn run(&mut self) {
         loop {
-            let byte_code = program[self.counter as usize];
+            let byte_code = self.mem_read(self.counter);
             self.counter += 1;
-            
-            let opcode = Opscode::try_from(byte_code);
 
-            if opcode.is_err() {
-                continue;
+            let opscode = Opscode::try_from(byte_code);
+
+            if opscode.is_err() {
+                return;
             }
 
-            match opcode.unwrap() {
-                Opscode::BRK_0x00 => {
-                    return;
-                },
-                Opscode::LDA_0xA9 => {
-                    // Gets param which is in counter after += (1 after function call)
-                    let param = program[self.counter as usize];
-                    self.counter += 1;
-                    self.register_a = param;
+            self.execute(opscode.unwrap())
+        }
+    }
 
-                    self.update_flag(Flag::Zero);
-                    self.update_flag(Flag::Negative);
-                },
-                Opscode::TAX_0xAA => {
-                    self.register_x = self.register_a;
+    fn execute(&mut self, opscode: Opscode) {
 
-                    self.update_flag(Flag::Zero);
-                    self.update_flag(Flag::Negative);
-                },
-                Opscode::INX_0xE8 => {
-                    self.register_x = self.register_x.wrapping_add(1);
+        match opscode {
+            Opscode::BRK_0x00 => {
+                // https://www.nesdev.org/obelisk-6502-guide/reference.html#BRK
+                return;
+            },
+            Opscode::LDA_0xA9 => {
+                // https://www.nesdev.org/obelisk-6502-guide/reference.html#LDA
+                // Gets param which is in counter after += (1 after function call)
+                let param = self.mem_read(self.counter);
+                self.counter += 1;
+                self.register_a = param;
 
-                    self.update_flag(Flag::Zero);
-                    self.update_flag(Flag::Negative);
-                }
+                self.update_flag(Flag::Zero);
+                self.update_flag(Flag::Negative);
+            },
+            Opscode::TAX_0xAA => {
+                // https://www.nesdev.org/obelisk-6502-guide/reference.html#TAX
+                self.register_x = self.register_a;
+
+                self.update_flag(Flag::Zero);
+                self.update_flag(Flag::Negative);
+            },
+            Opscode::INX_0xE8 => {
+                // https://www.nesdev.org/obelisk-6502-guide/reference.html#INX
+                self.register_x = self.register_x.wrapping_add(1);
+
+                self.update_flag(Flag::Zero);
+                self.update_flag(Flag::Negative);
             }
         }
     }
